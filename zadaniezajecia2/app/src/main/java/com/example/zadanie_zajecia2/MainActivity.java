c2e5532a074099230cc8257b6d0f824b909f206b
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-12-19 15:33:30 +0100

    adding authentication + firebase

--- zadaniezajecia2/app/src/main/java/com/example/zadanie_zajecia2/MainActivity.java
+++ zadaniezajecia2/app/src/main/java/com/example/zadanie_zajecia2/MainActivity.java
@@ -5,49 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-        MaterialButton registerbtn = (MaterialButton) findViewById(R.id.registerbtn);
-
-        //admin and admin
+        MaterialButton loginbtn = findViewById(R.id.loginbtn);
+        MaterialButton registerbtn = findViewById(R.id.registerbtn);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //correct
-                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                    Intent explicitIntent = new Intent(getApplicationContext(), MainActivity2.class);
-                    startActivity(explicitIntent);
+                String userEmail = username.getText().toString();
+                String userPassword = password.getText().toString();
 
-                }else
-                    //incorrect
-                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+                mAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                        .addOnCompleteListener(MainActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success
+                                Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                                Intent explicitIntent = new Intent(getApplicationContext(), MainActivity2.class);
+                                startActivity(explicitIntent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
         registerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent explicitIntent = new Intent(getApplicationContext(), MainActivity4.class);
-                    startActivity(explicitIntent);
-
-        }});
-
-}}
+                Intent explicitIntent = new Intent(getApplicationContext(), MainActivity4.class);
+                startActivity(explicitIntent);
+            }
+        });
+    }
+}
--- zadaniezajecia2/app/src/main/java/com/example/zadanie_zajecia2/MainActivity4.java
+++ zadaniezajecia2/app/src/main/java/com/example/zadanie_zajecia2/MainActivity4.java
@@ -1,14 +1,61 @@
 package com.example.zadanie_zajecia2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    // Declare Firebase authentication instance
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        EditText emailEditText = findViewById(R.id.mail);
+        EditText passwordEditText = findViewById(R.id.password);
+        EditText repeatPasswordEditText = findViewById(R.id.repat_password);
+        MaterialButton registerButton = findViewById(R.id.registerbtn);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String repeatPassword = repeatPasswordEditText.getText().toString();
+
+                if (!password.equals(repeatPassword)) {
+                    Toast.makeText(MainActivity4.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity4.this, task -> {
+                            if (task.isSuccessful()) {
+                                // Registration success
+                                Toast.makeText(MainActivity4.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                                Intent explicitIntent = new Intent(getApplicationContext(), MainActivity2.class);
+                                startActivity(explicitIntent);
+                            } else {
+                                // If registration fails, display a message to the user.
+                                Toast.makeText(MainActivity4.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 }
